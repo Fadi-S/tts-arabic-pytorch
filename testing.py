627b902ea8fb06fd5c111cb571bfb820ec0e2022
Fadi Sarwat <fadisarwat@yahoo.com>
2024-05-18 23:56:35 +0300

    Finish use

--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
 import torch
+from utils.data import text_mel_collate_fn
 
 pretrained = torch.load("pretrained/tacotron2_ar_adv.pth", map_location="cpu")
 ours = torch.load("states.pth", map_location="cpu")
--- use.py
+++ use.py
@@ -4,16 +4,21 @@ import soundfile as sf  # for saving audio files
 
 
 # model = Tacotron2Wave("pretrained/tacotron2_ar_adv.pth")
-model = Tacotron2Wave("states.pth")
+model = Tacotron2Wave("states_7000.pth")
 # model = model.cuda()
 
 import text
 
-line = "اهلا بيك في حلقة جديدة من برنامج الذكاء الصناعي"
-line2 = "ازيك عامل ايه يا باشا"
-line3 = "مساء الورد و الياسمين علي عنيكي الحلوين"
+lines = [
+    "اهلا بيك في حلقة جديدة من برنامج الذكاء الاقتصادي",
+    "ازيك عامل ايه يا باشا",
+    "مساء الورد و الياسمين علي عنيكي الحلوين",
+    "اهلا و سهلا",
+]
 
-wave, mel_spec = model.tts(text.arabic_to_buckwalter(line3), return_mel=True, denoise=0.005)
+for i in range(len(lines)):
+    line = lines[i]
+    wave, mel_spec = model.tts(text.arabic_to_buckwalter(line), return_mel=True, denoise=0.005)
 
-output_file_path = "output.wav"
-sf.write(output_file_path, wave, 22050)
+    output_file_path = f"output_{i}.wav"
+    sf.write(output_file_path, wave, 22050)
